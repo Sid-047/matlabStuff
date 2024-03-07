c6a2a4ac9be71a2ad4963a055512c950352500d4
Sid <siddiqmoideen07@gmail.com>
2024-03-07 14:43:05 +0530

    FeD a Bit of basicStuff

---
+++ basicsStuff/basicStuff.m
@@ -0,0 +1,27 @@
+x = 77;
+y = 11;
+
+format long
+k = pi;
+c = sin(k/2);
+
+z = sqrt(-27);
+
+ar1 = [2 77];
+
+ar2 = [[234 24] 
+       [2334 34]];
+
+columnVecAr = [77;88;99];
+rowVecAr = [44;55;33];
+
+format short
+matAr = [abs(-99) sqrt(125) 77;sqrt(-7) 99 pi^2];
+matArT = matAr';
+
+a = [11:33];
+b = [11:2:44];
+c = [1:0.5:3];
+
+p = linspace(0,1,10);
+
